78278b3c99ac734cfc606ffbf8142aad346f8138
chir263 <31371125+chir263@users.noreply.github.com>
2024-02-13 14:42:04 +0530

    version 1

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,8 @@
 // App.js or your main component where routes are defined
 import React from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+// import { HashRouter as Router, Route, Routes } from "react-router-dom";
+
+import { Routes, Route } from "react-router-dom";
 // import Callback from "./Callback";
 import Protected from "./Protected";
 import Callback from "./Callback";
@@ -19,16 +21,24 @@ const Default = () => {
   );
 };
 
+const Test = () => {
+  return (
+    <div>
+      <h1 className="text-4xl font-bold text-center m-4">Workflow App</h1>
+      tesing
+    </div>
+  );
+};
+
 const App = () => {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/dashboard" element={<Protected />} />
-        <Route path="/dashboard/:labname" element={<Protected />} />
-        <Route path="/" element={<Default />} />
-        <Route path="/callback" element={<Callback />} />
-      </Routes>
-    </BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Default />} />
+      <Route path="/dashboard" element={<Protected />} />
+      <Route path="/callback" element={<Callback />} />
+      <Route path="/test" element={<Test />} />
+      <Route path="/dashboard/:labname" element={<Protected />} />
+    </Routes>
   );
 };
 
